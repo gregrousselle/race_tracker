82fe6ca4af602f20f8303c762dedafea88c3fb74
gregrousselle <gregrousselle@gmail.com>
2020-11-17 16:35:06 +0100

    First Cesium integration

---
+++ SailR/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 2020
+
+// Statics
+app.use('/static',express.static(__dirname + '/static'));
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: __dirname });
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
